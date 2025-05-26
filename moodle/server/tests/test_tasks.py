33e9a002f749cf3a1154a9b196be65926e3210f0
Sergey Sokolov <sokolovserej@gmail.com>
2025-05-26 05:48:42 +0300

    [ADD] test random registers in tasks

--- moodle/server/tests/test_tasks.py
+++ moodle/server/tests/test_tasks.py
@@ -8,21 +8,22 @@ from tester.task4 import Task4
 
 
 class TestTasks(unittest.TestCase):
+
+    SEED = 139
+
     def test_task1(self):
         """
         Method checks sum task generator
         registers are non-negative
-        result is the sum of x1 and x2
+        result is the sum of two registers
         """
-        random.seed(139)
-
-        task1 = Task1('no_filename')
+        task1 = Task1('no_filename', self.SEED)
         for t in task1.tests:
-            regsum = t['reginit']['x1'] + t['reginit']['x2']
-            regres = t['expected_regs']['x3']
+            regsum = t['reginit'][task1.a_reg] + t['reginit'][task1.b_reg]
+            regres = t['expected_regs'][task1.res_reg]
 
-            assert t['reginit']['x1'] >= 0
-            assert t['reginit']['x2'] >= 0
+            assert t['reginit'][task1.a_reg] >= 0
+            assert t['reginit'][task1.b_reg] >= 0
 
             assert regsum == regres
             assert regsum != -regres or regres == 0
@@ -31,17 +32,16 @@ class TestTasks(unittest.TestCase):
         """
         Method checks gcd task generator
         registers are positive
-        result is the gcd of x10 and x11
+        result is the gcd of two registers
         """
-        random.seed(139)
 
-        task2 = Task2('no_filename')
+        task2 = Task2('no_filename', self.SEED)
         for t in task2.tests:
-            reggcd = math.gcd(t['reginit']['x10'], t['reginit']['x11'])
-            regres = t['expected_regs']['x10']
+            reggcd = math.gcd(t['reginit'][task2.a_reg], t['reginit'][task2.b_reg])
+            regres = t['expected_regs'][task2.res_reg]
 
-            assert t['reginit']['x10'] > 0
-            assert t['reginit']['x11'] > 0
+            assert t['reginit'][task2.a_reg] > 0
+            assert t['reginit'][task2.b_reg] > 0
             assert regres > 0
 
             assert reggcd == regres
@@ -53,18 +53,16 @@ class TestTasks(unittest.TestCase):
         register is positive
         result is n-th position of fib sequence
         """
-        random.seed(139)
-
         fib = [0, 1]
         for i in range(26): fib.append(fib[-1] + fib[-2])
 
-        task3 = Task3('no_filename')
+        task3 = Task3('no_filename', self.SEED)
         for t in task3.tests:
-            regpos = t['reginit']['x10']
-            assert t['reginit']['x10'] > 0
-            assert t['reginit']['x10'] < 25
+            regpos = t['reginit'][task3.n_reg]
+            assert t['reginit'][task3.n_reg] > 0
+            assert t['reginit'][task3.n_reg] < 25
 
-            regres = t['expected_regs']['x10']
+            regres = t['expected_regs'][task3.res_reg]
             assert fib[regpos] == regres
 
     def test_task4(self):
@@ -73,16 +71,14 @@ class TestTasks(unittest.TestCase):
         register is positive
         Result n!
         """
-        random.seed(139)
-
         fact = [1, 1]
         for k in range(2, 26): fact.append(fact[-1] * k)
 
-        task4 = Task4('no_filename')
+        task4 = Task4('no_filename', self.SEED)
         for t in task4.tests:
-            regpos = t['reginit']['x10']
-            assert t['reginit']['x10'] > 0
-            assert t['reginit']['x10'] < 25
+            regpos = t['reginit'][task4.n_reg]
+            assert t['reginit'][task4.n_reg] > 0
+            assert t['reginit'][task4.n_reg] < 25
 
-            regres = t['expected_regs']['x10']
+            regres = t['expected_regs'][task4.res_reg]
             assert fact[regpos] == regres
